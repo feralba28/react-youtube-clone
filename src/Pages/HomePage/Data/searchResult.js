7649d999cc077470a7730f61ea40f66cdb956471
Fernando Alba <fernando.alba@live.com.ar>
2022-01-24 18:45:49 -0300

    create data files to simulate data from backend

---
+++ src/Pages/HomePage/Data/searchResult.js
@@ -0,0 +1,216 @@
+// video list
+
+const searchResultItemsResponse = [
+  {
+    id: 'z95mZVUcJ-E',
+    snippet: {
+      publishedAt: '2020-10-19T23:00:14Z',
+      channelId: 'UCtoo4_P6ilCj7jwa4FmA5lQ',
+      title: 'Curso de JAVASCRIPT desde CERO (Completo) - Nivel JUNIOR',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/z95mZVUcJ-E/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/z95mZVUcJ-E/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/z95mZVUcJ-E/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+        standard: {
+          url: 'https://i.ytimg.com/vi/z95mZVUcJ-E/sddefault.jpg',
+          width: 640,
+          height: 480,
+        },
+        maxres: {
+          url: 'https://i.ytimg.com/vi/z95mZVUcJ-E/maxresdefault.jpg',
+          width: 1280,
+          height: 720,
+        },
+      },
+      channelTitle: 'Soy Dalto',
+    },
+    contentDetails: {
+      duration: 'PT10H41S',
+    },
+    statistics: {
+      viewCount: '1606374',
+    },
+  },
+  {
+    id: 'Q9fwkpxr3Dw',
+    snippet: {
+      publishedAt: '2019-09-17T13:00:06Z',
+      channelId: 'UCv85NiROLKddHa0fBATwTzw',
+      title: 'Aprende JavaScript en 15 Minutos 📒',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/Q9fwkpxr3Dw/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/Q9fwkpxr3Dw/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/Q9fwkpxr3Dw/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+        standard: {
+          url: 'https://i.ytimg.com/vi/Q9fwkpxr3Dw/sddefault.jpg',
+          width: 640,
+          height: 480,
+        },
+        maxres: {
+          url: 'https://i.ytimg.com/vi/Q9fwkpxr3Dw/maxresdefault.jpg',
+          width: 1280,
+          height: 720,
+        },
+      },
+      channelTitle: 'Victor Robles WEB',
+    },
+    contentDetails: {
+      duration: 'PT19M6S',
+    },
+    statistics: {
+      viewCount: '300502',
+    },
+  },
+  {
+    id: 'RqQ1d1qEWlE',
+    snippet: {
+      publishedAt: '2018-08-31T20:12:53Z',
+      channelId: 'UCX9NJ471o7Wie1DQe94RVIg',
+      title: 'Curso Javascript para Principiantes',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/RqQ1d1qEWlE/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/RqQ1d1qEWlE/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/RqQ1d1qEWlE/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+        standard: {
+          url: 'https://i.ytimg.com/vi/RqQ1d1qEWlE/sddefault.jpg',
+          width: 640,
+          height: 480,
+        },
+        maxres: {
+          url: 'https://i.ytimg.com/vi/RqQ1d1qEWlE/maxresdefault.jpg',
+          width: 1280,
+          height: 720,
+        },
+      },
+      channelTitle: 'Fazt',
+    },
+    contentDetails: {
+      duration: 'PT1H59M14S',
+    },
+    statistics: {
+      viewCount: '2328604',
+    },
+  },
+  {
+    id: 'PkZNo7MFNFg',
+    snippet: {
+      publishedAt: '2018-12-10T14:13:40Z',
+      channelId: 'UC8butISFwT-Wl7EV0hUK0BQ',
+      title: 'Learn JavaScript - Full Course for Beginners',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/PkZNo7MFNFg/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/PkZNo7MFNFg/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/PkZNo7MFNFg/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+        standard: {
+          url: 'https://i.ytimg.com/vi/PkZNo7MFNFg/sddefault.jpg',
+          width: 640,
+          height: 480,
+        },
+        maxres: {
+          url: 'https://i.ytimg.com/vi/PkZNo7MFNFg/maxresdefault.jpg',
+          width: 1280,
+          height: 720,
+        },
+      },
+      channelTitle: 'freeCodeCamp.org',
+    },
+    contentDetails: {
+      duration: 'PT3H26M43S',
+    },
+    statistics: {
+      viewCount: '8195995',
+    },
+  },
+  {
+    id: 'zBk-Hyv6ykw',
+    snippet: {
+      publishedAt: '2021-06-18T23:09:58Z',
+      channelId: 'UC55-mxUj5Nj3niXFReG44OQ',
+      title: 'Curso de Javascript en 40 minutos',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/zBk-Hyv6ykw/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/zBk-Hyv6ykw/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/zBk-Hyv6ykw/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+        standard: {
+          url: 'https://i.ytimg.com/vi/zBk-Hyv6ykw/sddefault.jpg',
+          width: 640,
+          height: 480,
+        },
+        maxres: {
+          url: 'https://i.ytimg.com/vi/zBk-Hyv6ykw/maxresdefault.jpg',
+          width: 1280,
+          height: 720,
+        },
+      },
+      channelTitle: 'Platzi',
+    },
+    contentDetails: {
+      duration: 'PT41M36S',
+    },
+    statistics: {
+      viewCount: '187306',
+    },
+  },
+]
+
+export default searchResultItemsResponse

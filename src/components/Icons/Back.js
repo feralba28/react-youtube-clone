963840607e8d0d5a001e9afb28361674cacdce1c
Fernando Alba <fernando.alba@live.com.ar>
2022-01-30 23:03:28 -0300

    create Icon components

---
+++ src/components/Icons/Back.js
@@ -0,0 +1,15 @@
+function Back(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={24}
+      width={24}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path d="M21 11v1H5.64l6.72 6.72-.71.71-7.93-7.93 7.92-7.92.71.71L5.64 11H21z" />
+    </svg>
+  )
+}
+
+export default Back

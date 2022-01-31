fbea97fc2e67234820f2886364bf12efc4ec40d8
Fernando Alba <fernando.alba@live.com.ar>
2022-01-30 23:07:34 -0300

    refactor WebsiteLayout using SearchBar component

--- src/layouts/WebsiteLayout.js
+++ src/layouts/WebsiteLayout.js
@@ -1,13 +1,25 @@
+import { useState } from 'react'
 import BottomNavigation from '../components/BottomNavigation/BottomNavigation'
 import Navbar from '../components/Navbar/Navbar'
+import SearchBar from '../components/SearchBar/SearchBar'
 
-function WebsiteLayout({ children }) {
+function WebsiteLayout({ children, setKeyword }) {
+  const [isSearchBar, setIsSearchBar] = useState(false)
+
+  const toggleSearchBar = () => setIsSearchBar(!isSearchBar)
+
+  const onSearch = (value) => {
+    setKeyword(value)
+    setIsSearchBar(!isSearchBar)
+  }
+
   return (
     <div className="container p-none d-flex fd-col min-height-100vh">
-      <Navbar />
-      <div className="container p-none grow-1">
-        {children}
-      </div>
+      <Navbar toggleSearchBar={toggleSearchBar} />
+      {isSearchBar && (
+        <SearchBar toggleSearchBar={toggleSearchBar} onSearch={onSearch} />
+      )}
+      <div className="container p-none grow-1">{children}</div>
       <BottomNavigation />
     </div>
   )

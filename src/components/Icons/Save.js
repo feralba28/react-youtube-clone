61647fbc4dc7a9ba559cb0470b7a504fda46eb05
Fernando Alba <fernando.alba@live.com.ar>
2022-02-08 11:57:37 -0300

    feat: create icon components

---
+++ src/components/Icons/Save.js
@@ -0,0 +1,16 @@
+function Save(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      enableBackground="new 0 0 24 24"
+      height={24}
+      viewBox="0 0 24 24"
+      width={24}
+      {...props}
+    >
+      <path d="M4,20h14v1H3V6h1V20z M18,10h-4V6h-1v4H9v1h4v4h1v-4h4V10z M21,3v15H6V3H21z M20,4H7v13h13V4z" />
+    </svg>
+  )
+}
+
+export default Save

0a6c0c49c24e575c2c3d1bcbe5f97afea970b7b9
Fernando Alba <fernando.alba@live.com.ar>
2022-02-09 10:21:44 -0300

    fix: replace images for icons componentes and add links to BottomNavigation

--- src/components/BottomNavigation/BottomNavigation.js
+++ src/components/BottomNavigation/BottomNavigation.js
@@ -1,28 +1,37 @@
+import Link from 'next/link'
+
+import Home from '../Icons/Home'
+import Explore from '../Icons/Explore'
+import Library from '../Icons/Library'
+
 import styles from './styles'
 
 function BottomNavigation() {
-  const homeIcon = '/images/home.svg'
-  const discoverIcon = '/images/discover.svg'
-  const libraryIcon = '/images/library.svg'
-  const discoverText = 'Explorar'
+  const exploreText = 'Explorar'
   const homeText = 'Principal'
   const libraryText = 'Biblioteca'
 
   return (
     <>
       <div className="row bg-white p-none border-top br-light-grey bottom-navigation">
-        <div className="col grow-1 p-none my-1 mx-4 d-flex fd-col ai-center">
-          <img src={homeIcon} alt={homeText} />
-          <p>{homeText}</p>
-        </div>
-        <div className="col grow-1 p-none my-1 mx-4 d-flex fd-col ai-center">
-          <img src={discoverIcon} alt={discoverText} />
-          <p>{discoverText}</p>
-        </div>
-        <div className="col grow-1 p-none my-1 mx-4 d-flex fd-col ai-center">
-          <img src={libraryIcon} alt={libraryText} />
-          <p>{libraryText}</p>
-        </div>
+        <Link href="/">
+          <div className="col grow-1 p-none my-1 mx-4 d-flex fd-col ai-center">
+            <Home className="vertical-align-middle" />
+            <p>{homeText}</p>
+          </div>
+        </Link>
+        <Link href="/explore">
+          <div className="col grow-1 p-none my-1 mx-4 d-flex fd-col ai-center">
+            <Explore className="vertical-align-middle" />
+            <p>{exploreText}</p>
+          </div>
+        </Link>
+        <Link href="/library">
+          <div className="col grow-1 p-none my-1 mx-4 d-flex fd-col ai-center">
+            <Library className="vertical-align-middle" />
+            <p>{libraryText}</p>
+          </div>
+        </Link>
       </div>
 
       <style jsx>{styles}</style>

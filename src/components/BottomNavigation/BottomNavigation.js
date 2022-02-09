c3722eec30582b5d3d3cc7fc4d1d8e9f1103bccf
Fernando Alba <fernando.alba@live.com.ar>
2022-02-09 10:53:21 -0300

    feat: add conditional rendering based on location pathname in BottomNavigation component

--- src/components/BottomNavigation/BottomNavigation.js
+++ src/components/BottomNavigation/BottomNavigation.js
@@ -1,12 +1,24 @@
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 import Home from '../Icons/Home'
 import Explore from '../Icons/Explore'
 import Library from '../Icons/Library'
+import HomeFilled from '../Icons/HomeFilled'
+import ExploreFilled from '../Icons/ExploreFilled'
+import LibraryFilled from '../Icons/LibraryFilled'
 
 import styles from './styles'
 
+const routes = {
+  home: '/',
+  explore: '/explore',
+  library: '/library',
+}
+
 function BottomNavigation() {
+  const { pathname } = useRouter()
+
   const exploreText = 'Explorar'
   const homeText = 'Principal'
   const libraryText = 'Biblioteca'
@@ -14,21 +26,33 @@ function BottomNavigation() {
   return (
     <>
       <div className="row bg-white p-none border-top br-light-grey bottom-navigation">
-        <Link href="/">
+        <Link href={routes.home}>
           <div className="col grow-1 p-none my-1 mx-4 d-flex fd-col ai-center">
-            <Home className="vertical-align-middle" />
+            {pathname === routes.home ? (
+              <HomeFilled className="vertical-align-middle" />
+            ) : (
+              <Home className="vertical-align-middle" />
+            )}
             <p>{homeText}</p>
           </div>
         </Link>
-        <Link href="/explore">
+        <Link href={routes.explore}>
           <div className="col grow-1 p-none my-1 mx-4 d-flex fd-col ai-center">
-            <Explore className="vertical-align-middle" />
+            {pathname === routes.explore ? (
+              <ExploreFilled className="vertical-align-middle" />
+            ) : (
+              <Explore className="vertical-align-middle" />
+            )}
             <p>{exploreText}</p>
           </div>
         </Link>
-        <Link href="/library">
+        <Link href={routes.library}>
           <div className="col grow-1 p-none my-1 mx-4 d-flex fd-col ai-center">
-            <Library className="vertical-align-middle" />
+            {pathname === routes.library ? (
+              <LibraryFilled className="vertical-align-middle" />
+            ) : (
+              <Library className="vertical-align-middle" />
+            )}
             <p>{libraryText}</p>
           </div>
         </Link>

ad88f6df1071f5303ab2e862fa604ec480a1e65f
Fernando Alba <fernando.alba@live.com.ar>
2022-02-10 12:35:57 -0300

    fix: pages and layout that use SearchBar component

--- src/layouts/WebsiteLayout.js
+++ src/layouts/WebsiteLayout.js
@@ -3,21 +3,16 @@ import BottomNavigation from '../components/BottomNavigation/BottomNavigation'
 import Navbar from '../components/Navbar/Navbar'
 import SearchBar from '../components/SearchBar/SearchBar'
 
-function WebsiteLayout({ children, setKeyword }) {
+function WebsiteLayout({ children }) {
   const [isSearchBar, setIsSearchBar] = useState(false)
 
   const toggleSearchBar = () => setIsSearchBar(!isSearchBar)
-
-  const onSearch = (value) => {
-    setKeyword(value)
-    setIsSearchBar(!isSearchBar)
-  }
 
   return (
     <div className="container p-none d-flex fd-col min-height-100vh">
       <Navbar toggleSearchBar={toggleSearchBar} />
       {isSearchBar && (
-        <SearchBar toggleSearchBar={toggleSearchBar} onSearch={onSearch} />
+        <SearchBar toggleSearchBar={toggleSearchBar} />
       )}
       <div className="container p-none grow-1">{children}</div>
       <BottomNavigation />

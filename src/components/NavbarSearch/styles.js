5bfad3f72c44684ae10befc9b01cab529e1f2537
Fernando Alba <fernando.alba@live.com.ar>
2022-02-20 01:26:08 -0300

    refactor NavbarSearch using tailwind css

--- src/components/NavbarSearch/styles.js
+++ src/components/NavbarSearch/styles.js
@@ -1,33 +1,13 @@
 import css from 'styled-jsx/css'
 
 const styles = css`
-  .navbar {
-    position: sticky;
-    top: 0;
-    z-index: 1;
-  }
-
-  .visible {
+  .nav-visible {
     transition: 0.3s;
   }
 
-  .hidden {
+  .nav-hidden {
     transition: 0.3s;
     transform: translateY(-100%);
-  }
-
-  .padding-search-input {
-    padding: 8px 12px;
-  }
-
-  .padding-search-filter {
-    padding: 4px 12px;
-  }
-
-  .microphone-bg {
-    width: 32px;
-    height: 32px;
-    border-radius: 50%;
   }
 `
 

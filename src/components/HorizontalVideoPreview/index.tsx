1c6afbf2cc42449be1778908af7d239165fdf5dc
Fernando Alba <fernando.alba@live.com.ar>
2022-02-20 01:47:17 -0300

    refactor HorizontalVideoPreview component using tailwind css

--- src/components/HorizontalVideoPreview/index.tsx
+++ src/components/HorizontalVideoPreview/index.tsx
@@ -15,8 +15,8 @@ function VideoPreview(props: { item: VideoPreviewItem }) {
   return (
     <>
       <Link href={`/watch/${item.id.videoId}`}>
-        <div className="row p-none px-2 ai-start mt-2">
-          <div className="col-5">
+        <div className="flex px-3 items-start mt-3">
+          <div className="w-5/12">
             <div className="preview-img-container">
               <img
                 src={item.snippet.thumbnails.high.url}
@@ -25,16 +25,18 @@ function VideoPreview(props: { item: VideoPreviewItem }) {
               />
             </div>
           </div>
-          <div className="col-7 p-none pl-2 d-flex ai-start">
-            <div className="grow-1">
-              <p className="fs-14 fw-400 preview-title">{item.snippet.title}</p>
-              <p className="fs-12 color-dark-grey">
+          <div className="w-7/12 pl-3 flex items-start">
+            <div className="grow">
+              <p className="text-sm text-gray-800 preview-title">
+                {item.snippet.title}
+              </p>
+              <p className="text-xs text-gray-400">
                 {item.snippet.channelTitle}
               </p>
-              <p className="fs-12 color-dark-grey">{timeAgo}</p>
+              <p className="text-xs text-gray-400">{timeAgo}</p>
             </div>
-            <div className='p-1'>
-              <Ellipsis className="vertical-align-middle" fill="#606060" />
+            <div className="p-1.5">
+              <Ellipsis fill="#9CA3AF" />
             </div>
           </div>
         </div>

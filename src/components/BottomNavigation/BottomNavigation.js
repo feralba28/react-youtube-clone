1ffe9dd77e4b6d2dd19194faf024dfafe4fb10eb
Fernando Alba <fernando.alba@live.com.ar>
2022-02-21 12:21:54 -0300

    fix: padding in BottomNavigation component

--- src/components/BottomNavigation/BottomNavigation.js
+++ src/components/BottomNavigation/BottomNavigation.js
@@ -23,35 +23,35 @@ function BottomNavigation() {
 
   return (
     <>
-      <div className="flex bg-white border sticky bottom-0">
+      <div className="flex bg-white sticky bottom-0">
         <Link href={routes.home}>
-          <div className="grow flex flex-col items-center p-1.5">
+          <div className="grow flex flex-col items-center p-1">
             {pathname === routes.home ? (
               <HomeFilled />
             ) : (
               <Home />
             )}
-            <p className="text-xs">{homeText}</p>
+            <p className="text-[11px]">{homeText}</p>
           </div>
         </Link>
         <Link href={routes.explore}>
-          <div className="grow flex flex-col items-center p-1.5">
+          <div className="grow flex flex-col items-center p-1">
             {pathname === routes.explore ? (
               <ExploreFilled />
             ) : (
               <Explore />
             )}
-            <p className="text-xs">{exploreText}</p>
+            <p className="text-[11px]">{exploreText}</p>
           </div>
         </Link>
         <Link href={routes.library}>
-          <div className="grow flex flex-col items-center p-1.5">
+          <div className="grow flex flex-col items-center p-1">
             {pathname === routes.library ? (
               <LibraryFilled />
             ) : (
               <Library />
             )}
-            <p className="text-xs">{libraryText}</p>
+            <p className="text-[11px]">{libraryText}</p>
           </div>
         </Link>
       </div>

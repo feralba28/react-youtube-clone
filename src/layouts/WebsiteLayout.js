5fae84c49538b99c7b4b270e2fb68bbd28fa43cb
Fernando Alba <fernando.alba@live.com.ar>
2022-02-28 11:07:16 -0300

    refactor WebsiteLayout for responsive design

--- src/layouts/WebsiteLayout.js
+++ src/layouts/WebsiteLayout.js
@@ -1,20 +1,36 @@
 import { useState } from 'react'
+
 import BottomNavigation from '../components/BottomNavigation/BottomNavigation'
 import Navbar from '../components/Navbar/Navbar'
-import SearchBar from '../components/SearchBar/SearchBar'
+import Sidebar from '../components/Sidebar'
+import SideNavigation from '../components/SideNavigation'
+
+import Menu from '../components/Icons/Menu'
 
 function WebsiteLayout({ children }) {
-  const [isSearchBar, setIsSearchBar] = useState(false)
+  const [isSidebar, setIsSidebar] = useState(false)
 
-  const toggleSearchBar = () => setIsSearchBar(!isSearchBar)
+  const toggleSidebar = () => setIsSidebar(!isSidebar)
 
   return (
-    <div className="container mx-auto flex flex-col h-screen">
-      <Navbar toggleSearchBar={toggleSearchBar} />
-      {isSearchBar && (
-        <SearchBar toggleSearchBar={toggleSearchBar} />
-      )}
-      <div className="grow">{children}</div>
+    <div
+      className="h-screen grid grid-cols-1 grid-rows-[48px_minmax(auto,_1fr)_50px]
+      lg:grid-cols-[74px_minmax(auto,_1fr)] lg:grid-rows-[54px_minmax(auto,_1fr)]"
+    >
+      <button className="bg-white sticky top-0 hidden lg:block" onClick={toggleSidebar}>
+        <Menu stroke="#303030"/>
+      </button>
+
+      <Navbar />
+      
+      <SideNavigation />
+
+      <Sidebar isSidebar={isSidebar} toggleSidebar={toggleSidebar} />
+
+      <main className='lg:border-t lg:overflow-y-scroll scroll-smooth'>
+        {children}
+      </main>
+
       <BottomNavigation />
     </div>
   )

eb4c9af20fcdf7860b8fb0d324220d45f5bc57f5
Fernando Alba <fernando.alba@live.com.ar>
2022-03-01 11:00:12 -0300

    refactor: styles in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,10 @@
-export default function Button(props: { icon?: JSX.Element; text: string }) {
-  const { icon, text } = props
+export default function Button(props: { icon?: JSX.Element; text: string, className?: string }) {
+  const { icon, text, className } = props
 
   return (
-    <button className="w-32 h-9 rounded-sm border border-blue-600 flex items-center justify-center gap-2">
+    <button className={`w-32 h-9 rounded-sm border border-blue-600 flex items-center justify-center gap-2 ${className}`}>
       {icon}
-      <p className="text-[14px] font-medium text-blue-600 uppercase">{text}</p>
+      <p className="text-[14px] font-medium text-blue-600 uppercase tracking-wide">{text}</p>
     </button>
   )
 }

a2899174979078e41ff1c366dda0146e65313f31
Fernando Alba <fernando.alba@live.com.ar>
2022-03-01 12:43:04 -0300

    refactor: navbar using useScroll hook and fix typos

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import Link from 'next/link'
 import useSearch from '../../hooks/useSearch'
+import useScroll from '../../hooks/useScroll'
 
 import SearchBar from '../SearchBar/SearchBar'
 import Suggestion from '../Suggestion/Suggestion'
@@ -17,41 +18,22 @@ import YouTube from '../Icons/YouTube'
 import styles from './styles'
 
 function Navbar({ toggleSidebar }) {
-  const [isNavbar, setIsNavbar] = useState(true)
-  const [oldScrollY, setOldScrollY] = useState(0)
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const newScrollY = window.scrollY
-
-      if (oldScrollY <= newScrollY) {
-        newScrollY >= 100 ? setIsNavbar(false) : setIsNavbar(true)
-      } else {
-        setIsNavbar(true)
-      }
-
-      setOldScrollY(newScrollY)
-    }
-
-    window.addEventListener('scroll', handleScroll)
-
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [oldScrollY])
+  const { isActive } = useScroll()
 
   const [isSearchBar, setIsSearchBar] = useState(false)
   const toggleSearchBar = () => setIsSearchBar(!isSearchBar)
 
-  const [value, setValue] = useState('')
-  const { suggestionResponse, navigate } = useSearch({ keyword: value })
+  const [inputValue, setInputValue] = useState('')
+  const { suggestionResponse, navigate } = useSearch({ keyword: inputValue })
 
-  const handleOnchange = (e) => setValue(e.target.value)
-  const handleSuggestionClick = (suggestion) => navigate(suggestion)
+  const handleOnchange = (e) => setInputValue(e.target.value)
+  const handleOnSuggestionClick = (suggestion) => navigate(suggestion)
 
   const inputRef = useRef()
 
   const handleOnSubmit = (e) => {
     e.preventDefault()
-    navigate(value)
+    navigate(inputValue)
   }
 
   const loginButtonText = 'Acceder'
@@ -61,7 +43,7 @@ function Navbar({ toggleSidebar }) {
     <>
       <nav
         className={`flex items-center justify-between gap-4 bg-white h-12 sticky top-0 shadow-md z-10 ${
-          isNavbar ? 'nav-visible' : 'nav-hidden'
+          isActive ? 'nav-visible' : 'nav-hidden'
         } md:col-span-2 md:shadow-none md:h-[56px] md:px-4`}
       >
         <div className="flex items-center gap-4">
@@ -99,16 +81,16 @@ function Navbar({ toggleSidebar }) {
                 type="search"
                 className="grow outline-none px-3 rounded-l-sm focus:border focus:border-blue-700 focus:px-[11px]"
                 placeholder={placeHolderText}
-                value={value}
+                value={inputValue}
                 onChange={handleOnchange}
               />
-              {suggestionResponse && value && (
+              {suggestionResponse && inputValue && (
                 <div className="absolute top-0 translate-y-[40px] w-full border shadow-lg divide-y divide-zinc-100">
                   {suggestionResponse[1].slice(0, 10).map((item, index) => (
                     <Suggestion
                       key={index}
                       item={item[0]}
-                      onSuggestionClick={handleSuggestionClick}
+                      onSuggestionClick={handleOnSuggestionClick}
                     />
                   ))}
                 </div>

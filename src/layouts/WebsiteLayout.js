43ae73a60ca41766ad802d001c12d9f260e80ceb
Fernando Alba <fernando.alba@live.com.ar>
2022-03-01 11:15:05 -0300

    refactor: WebsiteLayout for responsive design

--- src/layouts/WebsiteLayout.js
+++ src/layouts/WebsiteLayout.js
@@ -14,20 +14,23 @@ function WebsiteLayout({ children }) {
 
   return (
     <div
-      className="h-screen grid grid-cols-1 grid-rows-[48px_minmax(auto,_1fr)_50px]
-      md:grid-cols-[74px_minmax(auto,_1fr)] md:grid-rows-[54px_minmax(auto,_1fr)]"
+      className="min-h-screen flex flex-col
+      md:h-screen md:grid md:grid-cols-[74px_minmax(auto,_1fr)] md:grid-rows-[56px_minmax(auto,_1fr)]"
     >
-      <button className="bg-white sticky top-0 z-10 hidden md:block" onClick={toggleSidebar}>
-        <Menu stroke="#303030"/>
+      <button
+        className="bg-white sticky top-0 z-10 hidden md:block"
+        onClick={toggleSidebar}
+      >
+        <Menu stroke="#303030" />
       </button>
 
       <Navbar />
-      
+
       <SideNavigation />
 
       <Sidebar isSidebar={isSidebar} toggleSidebar={toggleSidebar} />
 
-      <main className='md:border-t md:col-span-2 md:overflow-y-scroll lg:col-span-1'>
+      <main className="grow md:border-t md:col-span-2 md:overflow-y-scroll lg:col-span-1">
         {children}
       </main>
 
